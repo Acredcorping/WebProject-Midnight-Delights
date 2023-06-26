1432d05d2f590f2d94bd8eb68a5d983f62a5c473
Acredcorping <13811412481@163.com>
2023-06-26 10:58:47 +0800

    init

---
+++ src/main/java/com/bjut/webmidnightdelights/WebMidnightDelightsApplication.java
@@ -0,0 +1,13 @@
+package com.bjut.webmidnightdelights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMidnightDelightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMidnightDelightsApplication.class, args);
+    }
+
+}
